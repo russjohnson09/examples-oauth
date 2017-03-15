d1154f6dde3763e0179b5d89fe71a4598fd8323e
russjohnson09 <russjohnson09@gmail.com>
2017-03-15 11:37:43 +0000

    angular example working

--- lowdb-oauth-example/server.js
+++ lowdb-oauth-example/server.js
@@ -1,1 +1,17 @@
 // https://github.com/typicode/lowdb
+
+
+
+var app = express();
+
+// Use the passport package in our application
+app.use(passport.initialize());
+
+// Create our Express router
+var router = express.Router();
+
+
+// Start the server
+var server = app.listen(process.env.PORT,function() {
+    console.log(server.address().port);
+});
